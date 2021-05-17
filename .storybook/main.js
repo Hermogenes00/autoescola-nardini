ab2de8fe12cfdf8f3f0ce4c908b5639f473ce3ea
unknown <neto.programer@gmail.com>
2021-05-16 22:38:06 -0300

    Add storybook,addons,docs,viewport and knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,9 +3,14 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
-}
+    "@storybook/addon-viewport",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
+    "@storybook/addon-knobs",
